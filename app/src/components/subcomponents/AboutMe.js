2d8153d917ce7a4d54077eeb899c894c2aaa08c1
Troy Jones <sntnmjones@gmail.com>
2020-04-04 14:58:00 -0700

    Under Construction

---
+++ app/src/components/subcomponents/AboutMe.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+class AboutMe extends Component {
+    constructor() {
+        super();
+        this.state = {};
+    }
+
+    render() {
+        return (
+
+            <p style={{textAlign: 'center'}}>Under Construction</p>
+
+        );
+    }
+}
+
+export default AboutMe;
