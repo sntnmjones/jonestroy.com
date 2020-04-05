6ec4ce110225845c0dd48961857d5af04691781d
Troy Jones <sntnmjones@gmail.com>
2020-04-05 09:52:43 -0700

    Removed md from master

--- app/src/components/homeComponents/Content.js
+++ app/src/components/homeComponents/Content.js
@@ -1,22 +1,13 @@
 import React, { Component } from 'react'
 import ReactMarkdown from 'react-markdown';
-import AboutMeMd from '../../markdown/aboutMe.md';
 
 class Content extends Component {
     constructor() {
         super();
-        this.state = {
-            markdown: ''
-        };
-    }
-
-    componentWillMount() {
-        // Get the contents from the Markdown file and put them in the React state, so we can reference it in render() below.
-        fetch(AboutMeMd).then(res => res.text()).then(text => this.setState({ markdown: text }));
+        this.state = {};
     }
 
     render() {
-        const { markdown } = this.state;
         return (
             <div style={{
                 /* Div */
@@ -25,7 +16,6 @@ class Content extends Component {
                 borderRight: '1px solid black',
                 padding: '5px',
             }}>
-                <ReactMarkdown source={markdown}  />
             </div>
         );
     }
